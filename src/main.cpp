2c3e9b9fa5fb123e0c3739c5b9277a230689fa3e
Miguel <mzorrilla@fi.uba.ar>
2026-07-18 05:20:58 -0300

    [Fix] agregando allowed user a main

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ int main() {
   try {
     std::string token(getenv("TOKEN"));
     printf("[ZeliBOT] Token: %s\n", token.c_str());
-    ZeliBot zelibot(token);
+    int allowed_user = std::stoi(getenv("ALLOWED_USER"));
+    ZeliBot zelibot(token, allowed_user);
     zelibot.run();
   } catch (std::exception &e) {
     std::cout << "[ERROR]" << e.what() << std::endl;
