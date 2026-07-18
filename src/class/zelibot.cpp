39aca573a3f97d62cd2abae248bc9d941b7f60a0
Miguel <mzorrilla@fi.uba.ar>
2026-07-18 05:11:07 -0300

    add condition for only one user

--- src/class/zelibot.cpp
+++ src/class/zelibot.cpp
@@ -2,22 +2,29 @@
 #include "tgbot/tgbot.h"
 #include <exception>
 #include <string>
-ZeliBot::ZeliBot(const std::string &token) : bot(token), long_poll(bot) {
+ZeliBot::ZeliBot(const std::string &token, const int allowed_user)
+    : bot(token), long_poll(bot), allowed_user(allowed_user) {
   initCommands();
 };
 
 void ZeliBot::initCommands() {
 
-  bot.getEvents().onCommand("start", [this](TgBot::Message::Ptr message) {
-    bot.getApi().sendMessage(message->chat->id, "Hi!");
-  });
-
   bot.getEvents().onCommand("test", [this](TgBot::Message::Ptr message) {
+    if (!is_allowed_user(message->chat->id)) {
+      bot.getApi().sendMessage(message->chat->id,
+                               "[ERROR] You are not allowed for use this bot.");
+      return;
+    }
     bot.getApi().sendMessage(message->chat->id, "Enter text");
     test_text_state = true;
   });
 
   bot.getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
+    if (!is_allowed_user(message->chat->id)) {
+      bot.getApi().sendMessage(message->chat->id,
+                               "[ERROR] You are not allowed for use this bot.");
+      return;
+    }
     if (test_text_state) {
       bot.getApi().sendMessage(message->chat->id, message->text);
       test_text_state = false;
@@ -46,4 +53,8 @@ void ZeliBot::run() {
   } catch (std::exception &e) {
     printf("error: %s\n", e.what());
   }
+}
+
+bool ZeliBot::is_allowed_user(const int chat_id) const {
+  return chat_id == allowed_user;
 }
--- src/includes/zelibot.hpp
+++ src/includes/zelibot.hpp
@@ -9,9 +9,12 @@ private:
   TgBot::TgLongPoll long_poll;
   const std::vector<std::string> bot_commands = {"start", "test"};
   bool test_text_state = false;
+  const int allowed_user;
+
+  bool is_allowed_user(const int chat_id) const;
 
 public:
-  ZeliBot(const std::string &token);
+  ZeliBot(const std::string &token, const int allowed_user);
 
   void initCommands();
 
